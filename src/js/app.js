f301af02c2b912993fd2ca306bd6b26e810cc05e
hunnyhu <huxiong888@163.com>
2015-12-10 23:27:57 +0800

    Add ngRoute

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,20 @@
     angular.element(document).ready(function () {
         console.log('page loading completed');
     });
-    var app = angular.module('app', ['app.directives', 'app.controllers']);
+    var app = angular.module('app', ['ngRoute', 'app.directives', 'app.controllers']);
     app.constant('PATH', {
       tpl: 'dest/tpl/'
     });
+    app.config(['$routeProvider', 'PATH', function($routeProvider, PATH) {
+        $routeProvider
+        .when('/list', {
+            templateUrl: PATH.tpl + 'home.tpl.html',
+            controller: 'homeController'
+        })
+        .otherwise({
+            redirectTo: '/list'
+        });
+    }]);
     // Attribute Isolated Scope
     app.run(function() {
         // Do post-load initialization stuff here
