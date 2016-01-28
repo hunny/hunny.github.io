58a151fd004c75574bfdfdc880c51e1aca81bf12
Hunny.Hu <ghunny57@gmail.com>
2016-01-28 15:45:14 +0800

    Nodejs nodemailer

---
+++ test/nodejs/sendemail.js
@@ -0,0 +1,44 @@
+var nodemailer = require('nodemailer');
+
+var smtpConfig = {
+    host: 'mail.server.com',
+    secure: true, // use SSL 
+    auth: {
+        user: 'your@email.com',
+        pass: 'yourpassword'
+    }
+};
+
+// create reusable transporter object using the default SMTP transport 
+var transporter = nodemailer.createTransport(smtpConfig);
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+// setup e-mail data with unicode symbols 
+var mailOptions = {
+    from: 'info@life.com', // sender address 
+    to: 'bb@bb.com,qq@qq.com,mm@mm.com', // list of receivers 
+    subject: 'Hello', // Subject line 
+    text: 'Hello world', // plaintext body 
+    html: '<b>Hello world</b>' // html body 
+};
+
+for (var i = 0; i < 1000; i ++) {
+	setTimeout(function() {
+		var mailOptions = {
+		    from: 'info@cdp-life.com', // sender address 
+		    to: 'xx@xx.com,zz@zz.com,cc@cc.com', // list of receivers 
+	        cc: 'ok@scg.co.th', // Comma separated list or an array
+		    subject: 'Hello ✔' + (i + 1), // Subject line 
+		    text: 'Hello world', // plaintext body 
+		    html: '<b>Hello world</b>' // html body 
+		};
+		// send mail with defined transport object 
+		transporter.sendMail(mailOptions, function(error, info) {
+		    if (error) {
+		        return console.log('Message[' + i + '] sent error: ' + error);
+		    }
+		    console.log('Message[' + i + '] sent: ' + info.response);
+		});
+	},  (i / 100));
+}
